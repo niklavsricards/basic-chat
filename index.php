d18f6e9845cc0c0aef6a914618fb3513e3f7c540
Niklavs <niklavsricardssablis@gmail.com>
2021-09-27 09:08:41 +0300

    fixed html

--- index.php
+++ index.php
@@ -24,16 +24,16 @@ $allMessages = $messageList->getMessages();
 
 ?>
 
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU"
+      crossorigin="anonymous">
+
 <?php foreach ($allMessages as $message): ?>
     <div class="alert alert-info w-25" role="alert">
         <?php echo $message->nickname() . ': ' . $message->message(); ?>
     </div>
 <?php endforeach; ?>
-
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-      integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU"
-      crossorigin="anonymous">
 
 <h3>Chat app</h3>
 
